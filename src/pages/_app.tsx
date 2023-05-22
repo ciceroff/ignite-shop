3766d0e35270df433d5495c0c2b8f73e91af24e5
ciceroff <cicero.barrozo13@gmail.com>
2023-05-22 16:47:04 -0300

    🎉 add home shirts

---
+++ src/pages/_app.tsx
@@ -0,0 +1,14 @@
+import { globalStyles } from "@/styles/global";
+import { Container, Header } from "@/styles/pages/app";
+import { AppProps } from "next/app";
+
+globalStyles();
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <Container>
+      <Header>Ignite Shop</Header>
+      <Component {...pageProps} />;
+    </Container>
+  );
+}
